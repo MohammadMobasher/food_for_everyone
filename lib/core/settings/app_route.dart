f8d26bc55bd05ee6741db6d3351ad3ffe9dcc2a2
mohammad <m.mobasher.z@gmail.com>
2023-04-07 16:44:03 +0430

    add route class

---
+++ lib/core/settings/app_route.dart
@@ -0,0 +1,26 @@
+import 'package:food_for_everyone/pages/splash/splash.dart';
+import 'package:get/get.dart';
+
+class AppRoute {
+  static const home = "/home";
+  static const splash = "/";
+  static const login = "/login";
+
+  static final routes = [
+    GetPage(
+      name: splash,
+      page: () => const Splash(),
+      opaque: false,
+    ),
+    // GetPage(
+    //   name: login,
+    //   page: () => const LoginPage(),
+    //   opaque: false,
+    // ),
+    // GetPage(
+    //   name: home,
+    //   page: () => Home(),
+    //   opaque: false,
+    // ),
+  ];
+}
