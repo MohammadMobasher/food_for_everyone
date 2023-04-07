9670ba864e152aa889f6b61dd7bbffb75b9844fc
mohammad <m.mobasher.z@gmail.com>
2023-04-07 16:44:37 +0430

    working on splash page

---
+++ lib/pages/splash/splash.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFF4B3A),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 56,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 49.sp),
+            child: ClipOval(
+              child: Container(
+                width: 73.sp,
+                height: 73.sp,
+                color: Colors.white,
+                child: Image.asset(
+                  "assets/images/splash_icon.png",
+                  width: 563.w,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 31.sp,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 51.sp),
+            child: const Text(
+              "Food for\nEveryone",
+              style: TextStyle(
+                fontSize: 65,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 400.sp,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 50.sp),
+            width: Get.width,
+            child: ElevatedButton(
+              onPressed: () {
+                // Get.toNamed(AppRoute.home);
+              },
+              style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 25.sp, horizontal: 104.sp),
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.sp),
+                  )),
+              child: Text(
+                "Get started",
+                style: TextStyle(
+                  fontSize: 17.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 36.sp,
+          )
+        ],
+      ),
+    );
+  }
+}
