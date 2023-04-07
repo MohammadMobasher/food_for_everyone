8fc935ace34bd6a946796326a9013264a25780ce
mohammad <m.mobasher.z@gmail.com>
2023-04-07 17:42:33 +0430

    add login item to app_route

--- lib/core/settings/app_route.dart
+++ lib/core/settings/app_route.dart
@@ -1,3 +1,4 @@
+import 'package:food_for_everyone/pages/login/login_page.dart';
 import 'package:food_for_everyone/pages/splash/splash.dart';
 import 'package:get/get.dart';
 
@@ -12,11 +13,11 @@ class AppRoute {
       page: () => const Splash(),
       opaque: false,
     ),
-    // GetPage(
-    //   name: login,
-    //   page: () => const LoginPage(),
-    //   opaque: false,
-    // ),
+    GetPage(
+      name: login,
+      page: () => const LoginPage(),
+      opaque: false,
+    ),
     // GetPage(
     //   name: home,
     //   page: () => Home(),
